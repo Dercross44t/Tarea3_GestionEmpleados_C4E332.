3c4e65e0d707ca4c7f36c35d81ab787bd34d7d51
Martin Angulo <chavezangulo9@gmail.com>
2026-05-01 14:32:51 -0600

    Se agrego el archivo IEmpleadoRepository al proyecto Tarea3.DA

---
+++ Tarea3.DA/Repositorios/IEmpleadoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tarea3.MODELS;
+
+namespace Tarea3.DA.Repositorios
+{
+    public interface IEmpleadoRepository
+    {
+        IEnumerable<Empleado> ObtenerTodos();
+        Empleado? ObtenerPorId();
+        IEnumerable<Empleado> BuscarPorNombreODepartamento(string termino);
+        IEnumerable<Empleado> ObtenerPaginado(int pagina, int tamano, string? busqueda);
+        int ContarTotalEmpleados(string? busqueda);
+        void Agregar(Empleado empleado);
+        void Actualizar(Empleado empleado);
+        void Eliminar(int id);
+    }
+}
