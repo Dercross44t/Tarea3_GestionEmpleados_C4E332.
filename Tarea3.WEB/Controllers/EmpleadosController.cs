f5c30416c09959dd394afdaaeb1a5feccfffe1ca
Martin Angulo <chavezangulo9@gmail.com>
2026-05-01 15:46:55 -0600

    Se modificaron los archivos de Home para poder usarlos con los empleados

--- Tarea3.WEB/Controllers/EmpleadosController.cs
+++ Tarea3.WEB/Controllers/EmpleadosController.cs
@@ -4,21 +4,20 @@ using Tarea3.MODELS;
 
 namespace Tarea3.WEB.Controllers
 {
-    public class EmpleadoController : Controller
+    public class EmpleadosController : Controller
     {
         private readonly IEmpleadoRepository _ropository;
         private const int TAMANO_MAX_PAGINAS = 5;
         private const int TAMANO_MIN_PAGINAS = 1;
         private const int SIN_PAGINAS = 0;
 
-        public EmpleadoController(IEmpleadoRepository repository)
+        public EmpleadosController(IEmpleadoRepository repository)
         {
             _ropository = repository;
         }
         public IActionResult Index(string? busqueda = "", int pagina = 1)
         {
-            if (pagina > TAMANO_MIN_PAGINAS) pagina = TAMANO_MIN_PAGINAS;
-
+            if (pagina < TAMANO_MIN_PAGINAS) pagina = TAMANO_MIN_PAGINAS;
             var empleado = _ropository.ObtenerPaginado(pagina, TAMANO_MAX_PAGINAS, busqueda);
             var totalRegistros = _ropository.ContarTotalEmpleados(busqueda);
             var totalPaginas = (int)Math.Ceiling((double) totalRegistros / TAMANO_MAX_PAGINAS);
